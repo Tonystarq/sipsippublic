047ebe280be1920c9428bd71a6e084d0ec1cae3f
Manish <manish.khdl.sy@gmail.com>
2022-03-28 16:11:39 +0530

    SipSip without paytm integration

---
+++ app/static/app/js/myscript.js
@@ -0,0 +1,123 @@
+$('#slider1, #slider2, #slider3').owlCarousel({
+    loop: true,
+    margin: 20,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false,
+            autoplay: true,
+        },
+        600: {
+            items: 3,
+            nav: true,
+            autoplay: true,
+        },
+        1000: {
+            items: 5,
+            nav: true,
+            loop: true,
+            autoplay: true,
+        }
+    }
+})
+
+$('.plus-cart').click(function () {
+    var id = $(this).attr("pid").toString();
+    var eml = this.parentNode.children[2];
+    console.log(id);
+    $.ajax(
+        {
+            type: "GET",
+            url: "/pluscart",
+            data: {
+                prod_id: id
+            },
+            success: function (data) {
+                console.log(data);
+                eml.innerText = data.quantity;
+                document.getElementById("amount").innerText = data.amount.toFixed(2);
+                document.getElementById("totalamount").innerText = data.totalamount.toFixed(2);
+            }
+        })
+});
+
+$('.minus-cart').click(function () {
+    var id = $(this).attr("pid").toString();
+    var eml = this.parentNode.children[2];
+    $.ajax(
+        {
+            type: "GET",
+            url: "/minuscart",
+            data: {
+                prod_id: id
+            },
+            success: function (data) {
+                console.log(data)
+                eml.innerText = data.quantity;
+                document.getElementById("amount").innerText = data.amount.toFixed(2);
+                document.getElementById("totalamount").innerText = data.totalamount.toFixed(2);
+            }
+        })
+});
+
+$('.remove-cart').click(function () {
+    var id = $(this).attr("pid").toString();
+    var elm = this;
+    $.ajax(
+        {
+            type: "GET",
+            url: "/removecart",
+            data: {
+                prod_id: id
+            },
+            success: function (data) {
+                console.log(data)
+                document.getElementById("amount").innerText = data.amount;
+                document.getElementById("totalamount").innerText = data.totalamount;
+                elm.parentNode.parentNode.parentNode.parentNode.remove()
+            }
+        })
+});
+
+$(document).ready(function () {
+
+    // toggle mobile menu
+    $('[data-toggle="toggle-nav"]').on('click', function () {
+        $(this).closest('nav').find($(this).attr('data-target')).toggleClass('hidden');
+        return false;
+    });
+
+    // feather icons
+    feather.replace();
+
+    // smooth scroll
+    var scroll = new SmoothScroll('a[href*="#"]');
+
+    // tiny slider
+    $('#slider-1').slick({
+        infinite: true,
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+    });
+
+    $('#slider-2').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        customPaging: function (slider, i) {
+            return '<div class="bg-white br-round w-1 h-1 opacity-50 mt-5" id=' + i + '> </div>'
+        },
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1
+            }
+        }, ]
+    });
+});
